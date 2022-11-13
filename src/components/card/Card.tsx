6d6ead15a153e5792f748566bb738589680c29fe
cesarauguto316 <cesaraugusto316@protonmail.com>
2022-11-12 19:28:08 -0500

    ⚡️ MedicineDetails.page almost completed

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -7,12 +7,12 @@ import './card.css';
 export const Card: FC<{ medicine: Medicine }> = ({ medicine }) => {
   return (
     <div className="card">
-      <figure className="card__image">
+      <Link className="card__image link" to={`medicine-details/${medicine.id}`}>
         <img src={medicine.photo.url} alt={medicine.title} />
-      </figure>
+      </Link>
 
       <div className="card__body">
-        <Link to={`medicine-details/${medicine.id}`}>
+        <Link className="link" to={`medicine-details/${medicine.id}`}>
           <h4 className="card__title">{medicine.title}</h4>
         </Link>
         <p className="card__descrtiption">{medicine.shortDescription}</p>
--- src/pages/medicineDetails/MedicineDetails.page.tsx
+++ src/pages/medicineDetails/MedicineDetails.page.tsx
@@ -1,21 +1,51 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import searchService from '../../services/search.service';
+import { FaArrowLeft } from 'react-icons/fa';
+import searchService, { Medicine } from '../../services/search.service';
+import './medicine-details.css';
 
 
 export const MedicineDetails: FC = () => {
   const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [medicine, setMedicine] = useState({} as Medicine);
 
   useEffect(() => {
-    searchService.getMedicineByID(id as string).then(res => {
-      console.log(res);
-    });
+    searchService.getMedicineByID(id as string)
+      .then(({ medicine }) => {
+        console.log(medicine);
+        setMedicine(medicine);
+      })
+      .catch(error => {
+        // use react-toastify
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <div className="medicine-details">
-      <h1>Medicine details</h1>
-      <Link to="/">Home</Link>
+      <h1 className="medicine-details__title">Medicine Details</h1>
+      <Link className="link medicine-details_left-arrow" to="/">
+        <FaArrowLeft />
+        <span>BACK</span>
+      </Link>
+
+      {isLoading && <h4>medicines are loading...</h4>}
+      {!isLoading && medicine && (
+        <div className="card-details">
+          <div className="card-details__body">
+            <h2 className="card-details__title">{medicine.title}</h2>
+            <p className="card-details__description">{medicine.description}</p>
+          </div>
+
+          <figure className="card-details__image">
+            <img src={medicine.photo.url} alt={medicine.title} />
+          </figure>
+        </div>
+      )}
     </div>
   );
 };
